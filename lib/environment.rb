6ef5df6e2b018e6bf6d0b8d901535ce1864be2a6
scratchglory <48923305+scratchglory@users.noreply.github.com>
2019-06-26 07:04:42 -0400

    first commit

---
+++ lib/environment.rb
@@ -0,0 +1,6 @@
+require "billboard/version"
+
+module Billboard
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/billboard_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Billboard do
+  it "has a version number" do
+    expect(Billboard::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
